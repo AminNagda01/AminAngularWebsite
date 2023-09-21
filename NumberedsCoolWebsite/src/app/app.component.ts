eb707662aed3405adbfe211f069afaa702b2be78
Muhammad Amin <aminworkm@gmail.com>
2023-09-21 17:53:34 -0400

    Continuing my personal work

--- NumberedsCoolWebsite/src/app/app.component.ts
+++ NumberedsCoolWebsite/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Numbereds Cool Website';
+  title = 'Numbereds Cool Website That I will work on 100 Percent';
 }
